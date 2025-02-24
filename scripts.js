a043af45cab15d37f98e020ad8194650f4c5bc17
Syrym-Joli <syrym.joli@gmail.com>
2025-02-24 17:47:21 +0500

    update

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
